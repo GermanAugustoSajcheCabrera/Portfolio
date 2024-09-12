b22b6817a893aeecc250ae259b9e73b3ce3f4f15
germansajche <sajchecabrera@gmail.com>
2024-09-12 12:16:27 -0600

    a

--- scripts.js
+++ scripts.js
@@ -6,52 +6,34 @@ menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-let slideIndex = 0;
-let autoSlideInterval = 6000; // 6 seconds
-
-showSlides(); // Initialize slideshow
-
-function showSlides() {
-    let i;
-    const slides = document.getElementsByClassName("image-box");
-    const dots = document.getElementsByClassName("dot");
-
-    // Hide all slides
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-
-    slideIndex++; // Move to the next slide
-    if (slideIndex > slides.length) {
-        slideIndex = 1; // Loop back to the first slide
-    }
-
-    // Remove "active" class from all dots
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    // Display the current slide and highlight the dot
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-
-    // Set a timer to automatically switch slides every 6 seconds
-    setTimeout(showSlides, autoSlideInterval);
-}
+let slideIndex = 1;
+showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    slideIndex += n - 1; // Adjust slideIndex to account for manual change
-    showSlides();
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    slideIndex = n - 1; // Adjust slideIndex to match selected dot
-    showSlides();
+    showSlides(slideIndex = n);
 }
 
-
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
 
 
 // Add smooth scrolling to all links
